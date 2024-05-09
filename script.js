010646a8a390620c4377d8dd9af1e86517cb4986
Juan Freire <ojuansfr9@gmail.com>
2024-05-09 14:58:45 -0300

    Adicionando animação ao scroll.

--- script.js
+++ script.js
@@ -65,3 +65,26 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowTamanho = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowTamanho;
+
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
