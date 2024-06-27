ec0dd3af046c611e6e1466901a36b25d143d9715
Juan Freire <ojuansfr9@gmail.com>
2024-06-27 11:57:30 -0300

    Transformando scrollSuave em classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initSmoothScroll from "./modules/scroll-suave.js";
+import SmoothScroll from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
@@ -10,7 +10,9 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
-initSmoothScroll();
+const smoothScroll = new SmoothScroll('[data-menu = "suave"] a[href^="#"]');
+smoothScroll.init();
+
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
